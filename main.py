00c3756a4414c58067e7f4b29e11e939fd872eec
yohany99 <yohany99@berkeley.edu>
2025-12-16 15:52:52 -0800

    edit get_book_text to take in sys.argv

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from stats import count_words, count_characters, sort_dictionary
+import sys
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    frankenstein_text = get_book_text("books/frankenstein.txt")
-    print(count_words(frankenstein_text))
-    char_map = count_characters(frankenstein_text)
-    print("--------- Character Count -------")
-    sorted_dicts = sort_dictionary(char_map)
-    for d in sorted_dicts:
-        print(d["char"] + ": " + str(d["count"]))
-    print("============= END ===============")
+    try:
+        text = get_book_text(sys.argv)
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(count_words(text))
+        char_map = count_characters(text)
+        print("--------- Character Count -------")
+        sorted_dicts = sort_dictionary(char_map)
+        for d in sorted_dicts:
+            print(d["char"] + ": " + str(d["count"]))
+        print("============= END ===============")
+    except Exception as e:
+        print(e)
+        return sys.exit(1)
 
 def get_book_text(file_path):
-    with open(file_path, encoding="utf-8") as f:
-        read_data = f.read()
-        return read_data
-
+    if len(file_path) < 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+    else:
+        with open(file_path[1], encoding="utf-8") as f:
+            read_data = f.read()
+            return read_data 
+            
 main()
